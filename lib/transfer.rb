7aac68ece5768867229d38b0fb8b655c813a70a9
JJKOrlino <jjkorlino80@yahoo.com>
2020-11-28 22:48:46 -0600

    commit tree 629bf94459e248e6c34c71c54258e1ad3a1d037c

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,73 @@
+# class Transfer
+
+#   attr_accessor :status
+
+#   def initialize(sender, receiver, amount)
+#     @sender = sender
+#     @receiver = receiver
+#     @amount = amount
+#     @status = 'pending'
+#   end
+
+#   def valid?
+#     @sender.valid? && @receiver.valid?
+#   end
+
+#   def execute_transaction
+#     if @sender.balance - @amount >= 0 && @status == 'pending' && @sender.valid? && @receiver.valid?
+#       @sender.balance -= @amount
+#       @receiver.balance += @amount
+#       @status = 'complete'
+#     else
+#       @status = 'rejected'
+#       "Transaction rejected. Please check your account balance."
+#     end
+#   end
+
+#   def reverse_transfer
+#     if @status == 'complete'
+#       @sender.balance += @amount
+#       @receiver.balance -= @amount
+#       @status = 'reversed'
+#     end
+#   end
+# end
+
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if @status == "pending" && @sender.balance > amount && sender.valid? && receiver.valid?
+      @sender.balance -= amount
+      @receiver.balance += amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete" && @sender.balance > amount && sender.valid? && receiver.valid?
+      @sender.balance += amount
+      @receiver.balance -= amount
+      @status = "reversed"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+
 end
